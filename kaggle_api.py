37f8750f76cc14f92ed8b5aefa35038c8ff67f25
zvonimir <zvonimirsucic.dl@gmail.com>
2024-10-24 17:23:01 +0200

    a

--- kaggle_api.py
+++ kaggle_api.py
@@ -2,10 +2,13 @@ import os
 
 from dotenv import load_dotenv
 from kaggle.api.kaggle_api_extended import KaggleApi
-
+import json
 load_dotenv()
 
 
 class KaggleApiBetter(KaggleApi):
     def __init__(self):
-        self._load_config({"username": os.environ["KAGGLE_USERNAME"], "key": os.environ["KAGGLE_KEY"]})
+        with open("kaggle.json", "r") as f:
+            kaggle_json = json.load(f)
+        self._load_config(kaggle_json)
+        #self._load_config({"username": os.environ["KAGGLE_USERNAME"], "key": os.environ["KAGGLE_KEY"]})
